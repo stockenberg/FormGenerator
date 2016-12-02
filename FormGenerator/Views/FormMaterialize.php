b5a3a68f9c637e2d33f739fff367d423650580a3
MaD <MStockenberg@googlemail.com>
2016-12-02 11:16:14 +0100

    example of implementing materializecss

--- FormGenerator/Views/FormMaterialize.php
+++ FormGenerator/Views/FormMaterialize.php
@@ -9,8 +9,6 @@
 namespace FormGenerator\Views;
 
 
-
-
 use FormGenerator\Inputs\FormMaster;
 use FormGenerator\Interfaces\Form;
 use FormGenerator\Interfaces\Input;
@@ -21,7 +19,18 @@ class FormMaterialize extends FormMaster
 {
     protected function prepareInput(Input $input)
     {
-        // TODO: Implement prepareInput() method.
+
+        switch ($input->getType()) {
+            case "checkbox":
+                return $this->switch($input);
+                break;
+
+            case "radio":
+                return $this->radio($input);
+                break;
+
+
+        }
 
     }
 
@@ -38,6 +47,28 @@ class FormMaterialize extends FormMaster
     protected function prepareForm(Form $form)
     {
         // TODO: Implement prepareForm() method.
+    }
+
+    private function switch(Input $input)
+    {
+        return "
+            <div class=\"switch {$input->getClasses()} \">
+                <label>
+                  <input id='{$input->getID()}' name='{$this->formid}[{$input->getName()}]' type=\"checkbox\">
+                  <span class=\"lever\"></span>
+                  {$input->getLabel()}
+                </label>
+              </div>
+        ";
+    }
+
+    private function radio(Input $input){
+        return "
+            <p>
+              <input name='{$this->formid}[{$input->getName()}]' type=\"radio\" id='{$input->getID()}' />
+              <label for='{$input->getID()}'>{$input->getLabel()}</label>
+            </p>
+        ";
     }
 
 
--- index.php
+++ index.php
@@ -7,26 +7,55 @@
  */
 require "vendor/autoload.php";
 
-$formFields = new \FormGenerator\Views\FormCustom("Register");
+$formFields = new \FormGenerator\Views\FormMaterialize("Register");
 
 $config = [
     [
         "label" => "Vorname",
         "name" => "firstname",
         "id" => "firstname",
-        "type" => "text",
+        "type" => "checkbox",
+        "element" => "Input"
+    ],
+    [
+        "label" => "PayPal",
+        "name" => "payment",
+        "id" => "paypal",
+        "type" => "radio",
+        "element" => "Input"
+    ],
+    [
+        "label" => "Sofort",
+        "name" => "payment",
+        "id" => "sofort",
+        "type" => "radio",
         "element" => "Input"
     ]
 
 ];
 
 $formFields->setConfig($config);
-echo $formFields->Output();
 
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
 
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+<?= $formFields->Output(); ?>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+</body>
+</html>
 
 
 
